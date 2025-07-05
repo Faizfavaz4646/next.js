36e3bbbd4b7794894fd4ea034e3792442254a1a2
Faiz Favaz <faizfavaz7@gmail.com>
2025-07-05 11:08:26 +0530

    file name changed

--- app/meals/[slug]/page.tsx
+++ app/meals/[slug]/page.tsx
@@ -6,6 +6,13 @@ import { notFound } from "next/navigation";
 interface Props {
   params: { slug: string };
 }
+type Meal={
+  id:number;
+  name:string;
+  slug: string;
+  description: string;
+  ingredients: string;
+}
 
 export default async function MealDetail({ params }: Props) {
   const res = await fetch(`http://localhost:5500/meals?slug=${params.slug}`, {
@@ -24,8 +31,10 @@ export default async function MealDetail({ params }: Props) {
       <Image
         src={meal.image}
         alt={meal.name}
-        className="w-full h-auto rounded-lg shadow mb-6">
-      </Image>
+        width={600}
+        height={400}
+        className="w-full h-auto rounded-lg shadow mb-6"
+      />
       <h1 className="text-2xl font-bold">{meal.name}</h1>
       <p className="mt-2 text-gray-700">{meal.description}</p>
       <h2 className="mt-4 font-semibold">Ingredients:</h2>
