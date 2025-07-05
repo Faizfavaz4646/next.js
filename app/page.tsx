b00678f0b5922e2777d6a6054e27ba5a03157c31
Faiz Favaz <faizfavaz7@gmail.com>
2025-07-05 11:22:10 +0530

    updated

--- app/page.tsx
+++ app/page.tsx
@@ -8,25 +8,25 @@ export default function Home() {
     <>
     
       <header className="bg-black text-center p-6">
-        <h1 className="text-6xl font-extrabold text-transparent bg-clip-text bg-gradient-to-r from-yellow-500 to-violet-500">
-          Next Level Food For Next Level Foodies
-        </h1>
-        <p className="text-white text-xl">Taste & share food from all over the world.</p>
-        
-        
-        <div className="flex justify-center gap-4 mt-5 text-xl">
-          <p className="text-black border rounded-lg  bg-clip- bg-gradient-to-r from-orange-500 to-orange-200 font-mono cursor-pointer px-4 py-2 hover:scale-105">
-            <Link href="/meals">Explore Meals</Link>
-          </p>
-          <p className="text-black border rounded-lg  bg-clip- bg-gradient-to-r from-orange-500 to-orange-200 font-mono cursor-pointer px-4 py-2 hover:scale-105">
-            <Link href="/meals/share">Share Meals</Link>
-          </p>
-          <p className="text-black border rounded-lg  bg-clip- bg-gradient-to-r from-orange-500 to-orange-200 font-mono cursor-pointer px-4 py-2 hover:scale-105">
-            <Link href="/community">Join the Community</Link>
-          </p>
-        </div>
-      
-      </header>
+  <h1 className="text-6xl font-extrabold text-transparent bg-clip-text bg-gradient-to-r from-yellow-500 to-violet-500">
+    Next Level Food For Next Level Foodies
+  </h1>
+  <p className="text-white text-xl">Taste &amp; share food from all over the world.</p>
+  <p className="text-white text-lg mt-2">Here&apos;s what we offer</p>
+
+  <div className="flex justify-center gap-4 mt-5 text-xl">
+    <p className="text-black border rounded-lg bg-clip- bg-gradient-to-r from-orange-500 to-orange-200 font-mono cursor-pointer px-4 py-2 hover:scale-105">
+      <Link href="/meals">Explore Meals</Link>
+    </p>
+    <p className="text-black border rounded-lg bg-clip- bg-gradient-to-r from-orange-500 to-orange-200 font-mono cursor-pointer px-4 py-2 hover:scale-105">
+      <Link href="/meals/share">Share Meals</Link>
+    </p>
+    <p className="text-black border rounded-lg bg-clip- bg-gradient-to-r from-orange-500 to-orange-200 font-mono cursor-pointer px-4 py-2 hover:scale-105">
+      <Link href="/community">Join the Community</Link>
+    </p>
+  </div>
+</header>
+
       <div className="bg-black"><Imageslideshow /></div>
          
      
